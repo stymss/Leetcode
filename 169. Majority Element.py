6b56f8758364bedf3fad1ddd9ecc7c462c23f864
Satyam Singh <crackcoder@outlook.com>
2024-12-02 17:31:51 +0530

    169. Majority Element

---
+++ 169. Majority Element.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Boyer Moore's Voting Algorithm
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate, votes = -1, 0
+        for num in nums:
+            if votes == 0:
+                candidate = num
+                votes = 1
+            else:
+                if num == candidate:
+                    votes += 1
+                else:
+                    votes -= 1
+        
+        # Check if the candidate is the majority element
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == candidate:
+                count += 1
+
+        return candidate if count >= len(nums)//2 else -1
