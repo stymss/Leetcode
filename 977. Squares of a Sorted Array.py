769ef9d96500275010d962f5e1c9cea6b11327b0
Satyam Singh <crackcoder@outlook.com>
2024-12-09 22:45:13 +0530

    977. Squares of a Sorted Array

--- 977. Squares of a Sorted Array.py
+++ 977. Squares of a Sorted Array.py
@@ -9,3 +9,21 @@ class Solution:
 
         return sorted(result)
     
+class Solution:
+    # TC - O(n)
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result = [0]*len(nums)
+        i, j, k = 0, len(nums)-1, len(nums)-1
+
+        # Check till we have exhausted the nums array
+        while i <= j:
+            i_sq, j_sq = nums[i]**2, nums[j]**2
+            if j_sq >= i_sq:
+                result[k] = j_sq
+                j-=1
+            else:
+                result[k] = i_sq
+                i+=1
+            k -= 1
+        
+        return result
