c3e6f5fc479d7728ddf05ee4631b4c4a135f8be6
Satyam Singh <codecracker@outlook.com>
2024-12-21 19:49:00 +0530

    268. Missing Number

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -10,3 +10,9 @@ class Solution:
             if idx != val:
                 return idx
         return len(nums) # If no numbers are missing, return the next number of the sequence
+    
+    def missingNumber(self, nums: List[int]) -> int:
+        ns = set(nums)
+        for i in range(len(nums)+1):
+            if i not in ns:
+                return i
